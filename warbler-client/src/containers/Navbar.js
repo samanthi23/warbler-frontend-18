bb7fa2e684e2947c370557b4054833b167bcf82e
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-04-12 15:29:58 +0000

    :hover links chrome dev tools

--- warbler-client/src/containers/Navbar.js
+++ warbler-client/src/containers/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+// import Logo
 import Logo from "../images/warbler-logo.png";
 
 class Navbar extends Component {
@@ -8,10 +9,14 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand">
         <div className="container-fluid">
+        { /* insert another header with navbar-header */ }
           <div className="navbar-header">
             <Link to="/" className="navbar-brand">
+             { /* insert logo here after importing it*/}
               <img src={Logo} alt="Warbler Home" />
             </Link>
+            
+          { /* line up horizontally signup and signin */ }
           </div>
           <ul className="nav navbar-nav navbar-right">
             <li>
@@ -21,7 +26,6 @@ class Navbar extends Component {
               <Link to="/signin">Log in</Link>
             </li>
           </ul>
-          )}
         </div>
       </nav>
     );
